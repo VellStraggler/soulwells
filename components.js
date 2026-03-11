74ee8a1d65965dca4733cd124336d3d237aadef0
VellStraggler <david@incwells.com>
2026-03-11 04:56:52 -0600

    new websites layout - more descriptive, and updated resume for download

--- components.js
+++ components.js
@@ -37,7 +37,74 @@ function Footer() {
     );
 }
 
-function PortfolioEntryText({title, description, link, github=null, isVideo=false}) {
+
+function Pipeline({ list }) {
+  if(list == null) return null;
+  const adjList = list;
+  const root = Object.keys(adjList)[0]; // first key as root
+  return <Node node={root} adjList={adjList} />;
+}
+
+function Node({ node, adjList }) {
+  const children = adjList[node] || [];
+
+  return (
+    <div className="node-container">
+      <div className="node">{node}</div>
+
+      {children.length > 0 && (
+        <div className="children">
+          {children.map(child => (
+            <div key={child} className="child-wrapper">
+              <Arrow />
+              <Node node={child} adjList={adjList} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Arrow() {
+  return <div className="arrow">↓</div>;
+}
+
+function Dates({dates}) {
+    if (dates ==null) return null;
+    return(
+        <p>Created in {dates}</p>
+    );
+}
+function Lighthouse({scores}) {
+    const titles=["Performance", "Accessibility", "Best Practices", "SEO"];
+    if(scores==null){
+        return null;
+    }
+    return (
+        <div>
+            <h2>Lighthouse Scores: </h2>
+            <div className="lighthouse-scores">
+                {scores.map((child, index) => (
+                    <div>
+                        {titles[index]}
+                        <div key={index} className="lighthouse-score">
+                            {child}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+function PortfolioEntryTitle({title}) {
+    return (
+        <div className="portfolio-text">
+            <h1>{title}</h1>
+        </div>
+    );
+}
+function PortfolioEntryText({description, link, github, isVideo, pipeline, lighthouse, dates}) {
     const linkText = isVideo ? <img width="32px" src="https://cdn.simpleicons.org/youtube/FF0000"/> : <img width="24px" src="assets/external-link-icon.png" />;
     let githubCode = null;
     if (github != null) {
@@ -47,13 +114,17 @@ function PortfolioEntryText({title, description, link, github=null, isVideo=fals
     }
     return (
         <div className="portfolio-text">
-            <h1>{title}</h1>
             <p>{description}</p>
-            <div className="centered-row">
-                {githubCode}
-                <a href={link}>
-                    {linkText}
-                </a>
+            <Pipeline list={pipeline}/>
+            <Lighthouse scores={lighthouse}/>
+            <div className="footer">
+                <Dates dates={dates}/>
+                <div>
+                    {githubCode}
+                    <a href={link}>
+                        {linkText}
+                    </a>
+                </div>
             </div>
         </div>
     );
@@ -61,27 +132,35 @@ function PortfolioEntryText({title, description, link, github=null, isVideo=fals
 
 function IFrame({link}) {
     return (
-        <iframe src={link} height="500px"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; web-share" allowFullScreen/>
+        <div className="iframe">
+            <iframe src={link}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; web-share" allowFullScreen/>
+        </div>
     );
 }
 
-function PortfolioEntry({title, description, link=null, github=null, isVideo=false}) {
+function PortfolioEntry({title, description, dates, link=null, github=null, isVideo=false, pipeline=null, lighthouse=null}) {
     return (
-        <div className="portfolio-entry">
-            <PortfolioEntryText title={title} description={description}
-                link={link} github={github} isVideo={isVideo}/>
-            <IFrame link={link} />
+        <div>
+            <PortfolioEntryTitle title={title}/>
+            <div className="portfolio-entry">
+                <PortfolioEntryText description={description}
+                    link={link} github={github} isVideo={isVideo} dates={dates} pipeline={pipeline} lighthouse={lighthouse}/>
+                <IFrame link={link} />
+            </div>
         </div>
 
     );
 }
-function PortfolioEntry2({title, description, link, github=null, isVideo=false}) {
+function PortfolioEntry2({title, description, dates, link=null, github=null, isVideo=false, pipeline=null, lighthouse=null}) {
     return (
-        <div className="portfolio-entry2">
-            <IFrame link={link} />
-            <PortfolioEntryText title={title} description={description}
-                link={link} github={github} isVideo={isVideo}/>
+        <div>
+            <PortfolioEntryTitle title={title}/>
+            <div className="portfolio-entry2">
+                <IFrame link={link} />
+                <PortfolioEntryText title={title} description={description}
+                    link={link} github={github} isVideo={isVideo} dates={dates} pipeline={pipeline} lighthouse={lighthouse}/>
+            </div>
         </div>
 
     );
--- websites.js
+++ websites.js
@@ -9,6 +9,14 @@ function WebsitesPage() {
 }
 
 function Websites() {
+    const pipeline1 = {
+    "GitHub React/Vite Site": ["AWS API Gateway", "Formspree"],
+    "AWS API Gateway": ["AWS Lambda"],
+    "AWS Lambda": ["AWS S3 (database)", "Braintree (payment)", "AWS SES (emailing)"]
+    };
+    const pipeline2 = {"Elementor Site": ["Formspree"]}
+    const pipeline3 = {"GitHub React Site": ["Formspree"]}
+    const pipeline4 = {"Weebly Site": ["Formspree"]}
     return (
         <div className="contents">
             <div className="card">
@@ -17,21 +25,30 @@ function Websites() {
                     <p>Here are official websites I've made for clients, particularly small business owners. Each one was made to 100% satisfaction, exactly what they wanted and more.</p>
                 </div>
                 <ul>
-                    <PortfolioEntry title="Scripture Creations E-Commerce Site" 
-                        description="This is a work in progress, scheduled to release February 23th, 2026. Made React with Vite, and converts a simple excel file for its product catalog. It's hosted on github and is planned to handle transactions using an optimized AWS Lambda function. Designed to be both affordable and fast."
+                    <PortfolioEntry title="Scripture Creations: E-Commerce Site" 
+                        description="This site had a simple goal: be an online store. Purchases are made. Receipts are auto-emailed. A simple push auto-publishes the site and updates the S3 bucket with a new product catalog. Designed to be both affordable and fast."
+                        pipeline={pipeline1}
+                        lighthouse={[96, 89, 100, 91]}
+                        dates="February 2026"
                         imgSrc="assets/scripture_preview.png" 
                         link="https://vellstraggler.github.io/Scripture-Creations/"
                         github="https://github.com/VellStraggler/Scripture-Creations"/>
                     <PortfolioEntry2 title="Edefying Official Site" 
                         description="Commissioned by Edefying and made using Elementor. Includes a fully functional contact-me page." 
-                        imgSrc="assets/edefying_preview.png" 
+                        imgSrc="assets/edefying_preview.png"
+                        pipeline={pipeline2}
+                        dates="November 2024"
                         link="https://edefying.com"/>
                     <PortfolioEntry title="TDL Proposed Site" 
                         description="Created first in HTML5 and then redone in Elementor, this was the first professional site I ever made. It was made with the intent to sell to TDL construction."
                         imgSrc="assets/tdl_preview.png" 
+                        pipeline={pipeline3}
+                        dates="September 2024"
                         link="https://tdl-construction.soulwells.us/"
                         github="https://github.com/VellStraggler/TDL-site"/>
-                    <PortfolioEntry2 title="Dara Luci Studio Official Site" description="This was made in Weebly in 5 hours. It's a portfolio site which includes a functional contact-me page"
+                    <PortfolioEntry2 title="Dara Luci Studio Official Site" description="This portfolio site is hosted on Weebly. Email collection work goes through Formspree. "
+                        pipeline={pipeline4}
+                        dates="January 2025"
                         imgSrc="assets/daraluci_preview.png" link="https://daraluci.weebly.com/"/>
                 </ul>
             </div>
